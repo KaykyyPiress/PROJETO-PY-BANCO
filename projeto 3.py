f07a3e77697e4623ea7e9480d4e76bda4b70cb23
Kayky Pires <125900277+KaykyyPiress@users.noreply.github.com>
2023-05-08 20:05:02 -0300

    pt3

--- projeto 3.py
+++ projeto 3.py
@@ -3,6 +3,7 @@ clientes = []
 
 #função que chama o cadastro com as perguntas
 def cadastro():
+
     nome = input("Qual seu nome? ")
     cpf = input("Qual seu CPF? Ex: 123.456.789-10 ")
     conta = input("Qual tipo de conta? Comum ou Plus? ")
@@ -23,6 +24,23 @@ def cadastro():
     print(pessoa)
     print("Registrado com sucesso!")
 
+def listar_clientes():
+    print("Lista de clientes cadastrados:")
+    for cliente in clientes:
+        print(f"Nome: {cliente['nome']}, CPF: {cliente['cpf']}, Tipo de conta: {cliente['tipo conta']}, Saldo: R${cliente['deposito']:.2f}")
+
+def debito():
+    cpf = input("Digite o CPF do cliente: ")
+    for cliente in clientes:
+        if cliente["cpf"] == cpf:
+            valor_debito = float(input("Qual o valor a ser debitado? "))
+            if valor_debito > cliente["deposito"]:
+                print("Saldo insuficiente.")
+            else:
+                cliente["deposito"] -= valor_debito
+                print(f"Débito de R${valor_debito:.2f} realizado com sucesso na conta de {cliente['nome']}.")
+            return
+    print("Cliente não encontrado.")
 
 def apaga_cliente():
     cpf = input("Digite o CPF do cliente que deseja apagar: ")
@@ -39,17 +57,25 @@ def apaga_cliente():
                 return
     print("Cliente não encontrado.")
 
-    
+def deposito():
+    cpf = input("Digite o CPF do cliente: ")
+    for cliente in clientes:
+        if cliente["cpf"] == cpf:
+            valor_deposito = float(input("Qual o valor a ser depositado? "))
+            cliente["deposito"] += valor_deposito
+            print(f"Depósito de R${valor_deposito:.2f} realizado com sucesso na conta de {cliente['nome']}.")
+            return
+    print("Cliente não encontrado.")
+
     
 #FUNÇÃO MENU PRINCIPAL
 def menu():
     while True:
         print("BEM VINDO AO BANCO TISTRESA: ")
-        opcao = int(input("1. Registrar Cliente\n2. Apagar Cliente\n9. Sair\n")) 
+        opcao = int(input("1. Registrar Cliente\n2. Apagar Cliente\n3. Depositar\n4. Mostrar Clientes\n5. Débito\n9. Sair\n")) 
         
         if opcao == 1:
             cadastro()
-            
             fim = input("Deseja Sair? S/N ")
             if fim.upper() == "S":
                 print(clientes)
@@ -58,6 +84,15 @@ def menu():
         
         elif opcao == 2:
             apaga_cliente()
+
+        elif opcao ==3:
+            deposito()
+
+        elif opcao == 4:
+            listar_clientes()
+
+        elif opcao == 5:
+            debito()
             
         elif opcao == 9:
             print("Obrigado por usar nosso banco")
